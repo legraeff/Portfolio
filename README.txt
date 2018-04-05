7ee20488a903df5e64c31860866c926a96cde621
legraeff <leticia.graeff@gmail.com>
2018-04-05 16:40:28 -0300

    just read the read.me...basically a first commit

--- README.txt
+++ README.txt
@@ -1,10 +1,5 @@
-A Portfolio page that showcases some of my projects. HTML, CSS and Bootstrap, JavaScript and JQuery.
-This may not be the most up to date version. You can see it at: http://codepen.io/legraeff/pen/obKqMx
+It should be a remake of my portfolio.
+It was a way to learn CSS Grids.
+It became...something.
 
-
-
-Project created for freecodecamp.com
-
-A Pen created at CodePen.io. You can find this one at http://codepen.io/legraeff/pen/obKqMx.
-
- 
+(I still am accepting ice-creams)
--- js/index.js
+++ js/index.js
@@ -1,44 +1,6 @@
-// Dropdown Toggle
+$( ".ice-cream" ).mouseenter(function() {
+  $(this).children(".ball").addClass( "ball-animation" );
+  $(this).children(".ball-drip").first(".ball-drip").addClass( "big-drip" );
+  $(this).children(".ball-drip").last(".ball-drip").addClass( "small-drip" );
 
-
-$("#toggle").click(function() {
-  $("#glyph-caret").toggleClass("fa-caret-up");
-});
-
-// Night/Day Toggle
-
-$("#togglesun").click(function() {
-  $("#glyph-sun").toggleClass("fa-moon-o");
-  $('body').toggleClass("night");
-  $('nav').toggleClass("navbar-inverse");
-
-});
-
-// Portfolio Slider 
-
-var currentIndex = 0,
-  items = $('.container div'),
-  total_items = items.length;
-
-function cycleItems() {
-  var item = $('.container div').eq(currentIndex);
-  items.hide();
-  item.css('display', 'inline-block');
-}
-
-var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > total_items - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-}, 3000);
-
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > total_items - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
 });
